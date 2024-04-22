03d045290120ff03924c189a91adc398beed4f8f
frarosset <37275581+frarosset@users.noreply.github.com>
2024-04-22 17:48:23 +0200

    Add function to get the fibonacci sequence of size n (recursive)
    The first n elements of the Fibonacci sequence are computed and
    returned as an array by function fibsRec(n).
    
    An recursive solution is used.

--- fibonacci.js
+++ fibonacci.js
@@ -14,7 +14,18 @@ function fibs(n){
     return seq;
 }
 
+// returns an array with the first n elements of the fibonacci sequence (recursive solution)
+function fibsRec(n){
+    if (n<3)
+        return [0,1].slice(0,n);
+
+    let seq = fibsRec(n-1);
+    seq.push(seq[n-2] + seq[n-3]);
+    return seq;
+}
+
+// test
 n_test = [0,1,2,3,4,5,6,7,8];
 for (let n of n_test){
-    console.log(n, fibs(n));
+    console.log(n, fibs(n), fibsRec(n));
 }
